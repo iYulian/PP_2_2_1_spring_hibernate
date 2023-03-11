fa46b49da0cd826fec19a22743608cdde068cbb2
iYulian <yulianlemur@gmail.com>
2023-03-11 16:10:00 +0300

    First commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,13 +21,20 @@ public class MainApp {
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Huawei", 123456)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Volkswagen", 234518)));
+      userService.add(new User("User7", "Lastname7", "user7@mail.ru", new Car("Mercedes", 321654)));
+      userService.add(new User("User8", "Lastname8", "user8@mail.ru", new Car("Chery", 147258)));
+
       List<User> users = userService.listUsers();
       for (User user : users) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         if (!(user.getCar() == null)) {
+            System.out.println(user.getCar());
+         }
       }
 
       context.close();
